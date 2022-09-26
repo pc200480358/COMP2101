984012bcd9762c033748e16fa086f6378f426c89
pc200480358 <113528512+pc200480358@users.noreply.github.com>
2022-09-26 16:14:27 -0400

    Create helloworldugly.sh

---
+++ bash/helloworldugly.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# My third script - helloworldugly.sh
+# This script displays the string “Hello World!”
+
+# This is a silly way of creating the output text by starting with something else and stream editing it in a pipeline
