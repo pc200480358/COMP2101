4273f73da6df5fe62ed5eedf6a5d96c4693dd93b
Sajana Tamang <tamangsajana97@gmail.com>
2022-09-30 11:14:16 -0400

    I want to send my file

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# going to bash directory
-#hostname -f
+#going to bash directory
+hostname -f
 #distinguish devices within local network
-#hostnamectl
-#peovide proper API and change related settings
-#hostname -l
+hostname ctl
+#provide proper API and change related settings
+hostname -l
 #distinguish devices within local network
-#df -h | grep -w /
+df -h | grep -w /
