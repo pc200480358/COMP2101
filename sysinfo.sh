60ea43b02fbb105edc70a4e83a4a1f4878182acf
Sajana Tamang <tamangsajana97@gmail.com>
2022-10-02 18:43:27 -0400

    modified my file

--- sysinfo.sh
+++ sysinfo.sh
@@ -4,7 +4,5 @@ hostname --fqdn
 #finding the hostname or dns domain name
 hostnamectl
 #finding the name and version of operating system
-ip addr
-#finding the ip address of the computer
-df
-#finding root filesystem free space in linux
+
+
