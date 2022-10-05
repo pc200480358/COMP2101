4fb01fd42882a73dd5483d66ec80daea4c54d708
pc200480358 <113528512+pc200480358@users.noreply.github.com>
2022-10-05 10:51:24 -0400

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
-#going to bash directory
-hostname --fqdn
-#finding the hostname or dns domain name
+#! /bin/bash
+#going to the bash directory 
+echo "FQDN: " $(hostname --fqdn)
+#showing fqdn details
+hostname -f
+#showing hostnam details
 hostnamectl
-#finding the name and version of operating system
-
-
+#hosname changes
+echo "IP Addresses: "$(hostname -I)
+#showing ip address
+echo ""
+#showing storage info for root
+echo "Root system info: "
+echo $(df -h | sed -n 1p)
+echo $(df -h | sed -n 3p)
