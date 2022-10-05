ff64c2b039370f6e5834d028709a34d89820962f
pc200480358 <113528512+pc200480358@users.noreply.github.com>
2022-10-05 09:00:06 -0400

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,8 +2,8 @@
 
 echo -e "report for myvm"
 echo ===============
-echo -e "fqdn:\t\t"`hostname --fqdn`
-echo -e `hostnamectl | grep "Operating System"`
-echo -e "System Main IP:\t\t"`hostname -I`
-echo -e "Root Filesystem Free Space:\t\t" `df -h /root`
+echo -e "fqdn:\t\t"`hostname --fqdn` #finding fqdn
+echo -e `hostnamectl | grep "Operating System"`#finding name and version of OS
+echo -e "System Main IP:\t\t"`hostname -I` #finding ip address of device
+echo -e "Root Filesystem Free Space:\t\t" `df -h /root` #finding disk space
 echo ===============
