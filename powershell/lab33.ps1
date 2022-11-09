419d0c5f9772863a04fe6488e8ae815abf215856
pc200480358 <113528512+pc200480358@users.noreply.github.com>
2022-11-08 21:48:39 -0500

    Create lab33.ps1

---
+++ powershell/lab33.ps1
@@ -0,0 +1,3 @@
+﻿Get-Ciminstance -Classname win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "true"} |
+Format-Table -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchorder
